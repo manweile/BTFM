a51ee44fa7a3e494ce7af6786477023544832b20
manweile <manweiler@ualberta.ca>
2024-03-24 17:32:12 -0600

    udaptes

--- Sketch1/Debug/ArduinoSTL/valarray.cpp.d
+++
@@ -1,7 +0,0 @@
-C:\\Users\\Gerald\\AppData\\Local\\Temp\\VMBuilds\\Sketch1\\mega_atmega2560\\Debug\\ArduinoSTL\\valarray.cpp.o: \
- c:\users\gerald\appdata\local\temp\vmbuilds\sketch1\mega_atmega2560\debug\src\_micro-api\libraries\arduinostl\src\valarray.cpp \
- f:\myarduino\projects\libraries\arduinostl\src\valarray \
- f:\myarduino\projects\libraries\arduinostl\src\basic_definitions \
- f:\myarduino\projects\sketch1\sketch1\src\_micro-api\libraries\arduinostl\src\system_configuration.h \
- f:\myarduino\projects\libraries\arduinostl\src\cstddef \
- f:\myarduino\projects\libraries\arduinostl\src\cmath
--- Sketch1/Release/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\\Users\\Gerald\\AppData\\Local\\Temp\\VMBuilds\\Sketch1\\mega_atmega2560\\Release\\Wire.cpp.o: \
- c:\users\gerald\appdata\local\arduino15\packages\arduino\hardware\avr\1.8.2\libraries\wire\src\wire.cpp \
- c:\users\gerald\appdata\local\arduino15\packages\arduino\hardware\avr\1.8.2\libraries\wire\src\utility/twi.h \
- c:\users\gerald\appdata\local\arduino15\packages\arduino\hardware\avr\1.8.2\libraries\wire\src\Wire.h \
- c:\users\gerald\appdata\local\arduino15\packages\arduino\hardware\avr\1.8.2\cores\arduino\stream.h \
- c:\users\gerald\appdata\local\arduino15\packages\arduino\hardware\avr\1.8.2\cores\arduino\Print.h \
- c:\users\gerald\appdata\local\arduino15\packages\arduino\hardware\avr\1.8.2\cores\arduino\WString.h \
- c:\users\gerald\appdata\local\arduino15\packages\arduino\hardware\avr\1.8.2\cores\arduino\Printable.h
