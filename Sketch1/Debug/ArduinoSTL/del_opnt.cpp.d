a2134cde39ebfb543251882543a0e40f1a51ddab
manweile <manweiler@ualberta.ca>
2024-03-24 17:44:51 -0600

    untracking

--- Sketch1/Debug/ArduinoSTL/del_opnt.cpp.d
+++
@@ -1,9 +0,0 @@
-C:\\Users\\Gerald\\AppData\\Local\\Temp\\VMBuilds\\Sketch1\\mega_atmega2560\\Debug\\ArduinoSTL\\del_opnt.cpp.o: \
- c:\users\gerald\appdata\local\temp\vmbuilds\sketch1\mega_atmega2560\debug\src\_micro-api\libraries\arduinostl\src\del_opnt.cpp \
- f:\myarduino\projects\libraries\arduinostl\src\new \
- f:\myarduino\projects\libraries\arduinostl\src\basic_definitions \
- f:\myarduino\projects\sketch1\sketch1\src\_micro-api\libraries\arduinostl\src\system_configuration.h \
- f:\myarduino\projects\libraries\arduinostl\src\exception \
- f:\myarduino\projects\libraries\arduinostl\src\cstddef \
- f:\myarduino\projects\libraries\arduinostl\src\cstdlib \
- f:\myarduino\projects\libraries\arduinostl\src\func_exception
--- Sketch1/Debug/ArduinoSTL/del_opv.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\\Users\\Gerald\\AppData\\Local\\Temp\\VMBuilds\\Sketch1\\mega_atmega2560\\Debug\\ArduinoSTL\\del_opv.cpp.o: \
- c:\users\gerald\appdata\local\temp\vmbuilds\sketch1\mega_atmega2560\debug\src\_micro-api\libraries\arduinostl\src\del_opv.cpp
